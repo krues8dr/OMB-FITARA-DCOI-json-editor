434337eb2a7162757d9c7fdbf0a7018f3ae62673
Alex Perfilov <alexandr.perfilov@reisystems.com>
2016-06-09 00:57:28 -0400

    prepopulating data, moving descriptions under input, better logic

--- src/app.js
+++ src/app.js
@@ -2,63 +2,87 @@ var React = require('react');
 
 var ReactDOM = require('react-dom');
 var JSONSchemaForm = require('react-jsonschema-form');
-var schema = require('./js/schema.js');
-var uiSchema = require('./js/uiSchema.js');
-
+var DCOIschema = require('./js/schema.js');
+var formUiSchema = require('./js/uiSchema.js');
 var dummyData = require('./js/dummy.js');
+
+const Form = JSONSchemaForm.default;
+
+// var version = require('../package.json').version;
+var version = '0.0.1';
+
+function walk(schema, uiSchema, formData) {
+    if (schema.hasOwnProperty('properties')) {
+        for (var key in schema.properties) {
+            if (schema.properties.hasOwnProperty(key)){
+                uiSchema[key] = uiSchema[key] || {};
+                if (schema.properties[key].hasOwnProperty('properties')) {
+                    formData[key] = formData[key] || {};
+                } else {
+                    if (schema.properties[key].hasOwnProperty('minimum') && schema.properties[key].hasOwnProperty('maximum')){
+                        if (schema.properties[key].minimum == schema.properties[key].maximum) {
+                            formData[key] = schema.properties[key].minimum;
+                        }
+                    }
+                }
+                walk(schema.properties[key], uiSchema[key], formData[key]);
+            }
+        }
+    } else {
+        if (schema.hasOwnProperty('description')) {
+            uiSchema['ui:help'] = uiSchema['ui:help'] || schema.description;
+            schema.description = '';
+        }
+    }
+
+}
+
+const initialFormData = {};
+
+walk(DCOIschema, formUiSchema, initialFormData);
+
+console.log(formUiSchema);
+console.log(initialFormData);
 
 // Covering browsers without Object.assign support (IE 9-11)
 require('./js/polyfill.js');
 
-var generateJson = function generateJson(data) {
-    ReactDOM.render(React.createElement('textarea', {
+var App = React.createClass({
+    displayName: 'App',
+
+    getInitialState: function () {
+        return {
+            schema: DCOIschema,
+            uiSchema: formUiSchema,
+            formData: initialFormData,
+            liveValidate: true,
+            output: '',
+            inputForm: ''
+        };
+    },
+
+    generateJson: function (data) {
+        var out = React.createElement('textarea', {
             className: "form-control",
-            rows: 20,
             value: JSON.stringify(data.formData, null, '\t'),
             readOnly: true
-        }, null)
-        , document.getElementById("out"));
-};
+        });
+        this.setState({output: out});
+    },
 
-var emptyOutput = function emptyOutput() {
-    ReactDOM.render(React.createElement('br', {}, null)
-        , document.getElementById("out"));
-};
+    loadDummyData: function () {
+        if (this.state.formData == initialFormData || confirm('Your form data will be replaced with dummy data, so your changes will be lost. Are you sure you want to proceed?')) {
+            this.setState({formData: dummyData});
+        }
+    },
 
-var loadDummyData = function loadDummyData() {
-    ReactDOM.render(
-        React.createElement(JSONSchemaForm.default, {
-            schema: schema,
-            uiSchema: uiSchema,
-            formData: dummyData,
-            liveValidate: true,
-            onChange: emptyOutput,
-            onSubmit: generateJson,
-            onError: emptyOutput
-        }), document.getElementById("app"));
-};
+    loadMyData: function() {
+        var data = JSON.parse(document.getElementById("json-input").value);
+        this.setState({formData: data, inputForm: ''});
+    },
 
-var loadMyData = function loadMyData() {
-    var data = JSON.parse(document.getElementById("json-input").value);
-    ReactDOM.render(
-        React.createElement(JSONSchemaForm.default, {
-            schema: schema,
-            uiSchema: uiSchema,
-            formData: data,
-            liveValidate: true,
-            onChange: emptyOutput,
-            onSubmit: generateJson,
-            onError: emptyOutput
-        }), document.getElementById("app"));
-    ReactDOM.render(
-        React.createElement('div')
-        ,document.getElementById("input")
-    );
-};
-
-var submitMyData = function submitMyData() {
-    ReactDOM.render(
-        React.createElement("div",
+    showLoadMyDataForm: function() {
+        const myDataForm = React.createElement("div",
             {className: "form-group field field-object"},
             React.createElement("h4",
                 {}, "Please enter your json:"
@@ -73,121 +97,90 @@ var submitMyData = function submitMyData() {
                 {className: "text-center"},
                 React.createElement("button",
                     {
-                        onClick: loadMyData,
+                        onClick: this.loadMyData,
                         className: "btn btn-primary"
                     },
                     "Load data"
                 ))
-        )
-        , document.getElementById("input"));
-};
+        );
+        this.setState({inputForm: myDataForm});
+    },
+
+    onFormDataChange: function(obj){
+        this.setState({formData: obj.formData, output:''})
+    },
+
+    render: function render() {
+        const {
+            schema,
+            uiSchema,
+            formData,
+            liveValidate,
+            output,
+            inputForm
+        } = this.state;
+
+        return React.createElement(
+            'div',
+            {className: 'container'},
+            [
+                React.createElement(
+                    'div',
+                    {
+                        id: 'controls',
+                        className: 'row',
+                        key: 'ctrls'
+                    },
+                    React.createElement("button", {
+                        className: "btn btn-danger pull-right",
+                        onClick: this.loadDummyData,
+                        key: 'dummy'
+                    }, "Load dummy data (test)"),
+                    React.createElement("button", {
+                        className: "btn btn-primary pull-left",
+                        onClick: this.showLoadMyDataForm,
+                        key: 'mydata'
+                    }, "Load my own data")
+                ),
+                React.createElement(
+                    'div',
+                    {
+                        id: 'input',
+                        className: 'row my-data',
+                        key: 'input'
+                    },
+                    inputForm
+                ),
+                React.createElement(
+                    Form,
+                    {
+                        schema: schema,
+                        uiSchema: uiSchema,
+                        formData: formData,
+                        liveValidate: liveValidate,
+                        onChange: this.onFormDataChange,
+                        onSubmit: this.generateJson,
+                        key: 'form'
+                    }
+                ),
+                React.createElement(
+                    'div',
+                    {
+                        id: 'out',
+                        className: 'row out',
+                        key: 'out'
+                    },
+                    output
+                )
+            ]
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(
+    App
+), document.getElementById('app'));
 
 (0, ReactDOM.render)(
-    React.createElement("div", {
-            className: "row"
-        },
-        React.createElement("button", {
-            className: "btn btn-danger pull-right",
-            onClick: loadDummyData
-        }, "Load dummy data (test)"),
-        React.createElement("button", {
-            className: "btn btn-primary pull-left",
-            onClick: submitMyData
-        }, "Load my own data")
-    ), document.getElementById("controls"));
-
-(0, ReactDOM.render)(
-    React.createElement(JSONSchemaForm.default, {
-        schema: schema,
-        uiSchema: uiSchema,
-        liveValidate: true,
-        onChange: emptyOutput,
-        onSubmit: generateJson,
-        onError: emptyOutput
-    }), document.getElementById("app"));
-
-
-// const Form = JSONSchemaForm.default;
-//
-// var App = React.createClass({
-//     constructor(props) {
-//         super(props);
-//         // initialize state with Simple data sample
-//         const {schema, uiSchema, formData, validate} = samples.Simple;
-//         this.state = {
-//             form: false,
-//             schema,
-//             uiSchema,
-//             formData,
-//             validate,
-//             editor: "default",
-//             theme: "default",
-//             liveValidate: true,
-//         };
-//     },
-//     render() {
-//         const {
-//             schema,
-//             uiSchema,
-//             formData,
-//             liveValidate,
-//             validate,
-//             theme,
-//             editor
-//         } = this.state;
-//
-//         return (
-//             <div className="container-fluid">
-//                 <div className="page-header">
-//                     <h1>react-jsonschema-form</h1>
-//                     <div className="row">
-//                         <div className="col-sm-8">
-//                             <Selector onSelected={this.load}/>
-//                         </div>
-//                         <div className="col-sm-2">
-//                             <Form schema={liveValidateSchema}
-//                                   formData={liveValidate}
-//                                   onChange={this.setLiveValidate}>
-//                                 <div/>
-//                             </Form>
-//                         </div>
-//                         <div className="col-sm-2">
-//                             <ThemeSelector theme={theme} select={this.onThemeSelected}/>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <div className="col-sm-7">
-//                     <Editor title="JSONSchema" theme={editor} code={toJson(schema)}
-//                             onChange={this.onSchemaEdited}/>
-//                     <div className="row">
-//                         <div className="col-sm-6">
-//                             <Editor title="UISchema" theme={editor} code={toJson(uiSchema)}
-//                                     onChange={this.onUISchemaEdited}/>
-//                         </div>
-//                         <div className="col-sm-6">
-//                             <Editor title="formData" theme={editor} code={toJson(formData)}
-//                                     onChange={this.onFormDataEdited}/>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <div className="col-sm-5">
-//                     {!this.state.form ? null :
-//                         <Form
-//                             liveValidate={liveValidate}
-//                             schema={schema}
-//                             uiSchema={uiSchema}
-//                             formData={formData}
-//                             onChange={this.onFormDataChange}
-//                             fields={{geo: GeoPosition}}
-//                             validate={validate}
-//                             onError={log("errors")}/>}
-//                 </div>
-//             </div>
-//         );
-//     }
-// });
-//
-// ReactDOM.render(
-//     React.createElement(App, null),
-//     document.getElementById('app')
-// );
+    React.createElement("em", {}, "version "+version)
+    , document.getElementById("version"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-var path = require("path");
 const webpack = require('webpack');
 
 module.exports = {
@@ -11,17 +10,17 @@ module.exports = {
         new webpack.IgnorePlugin(/^(buffertools)$/)
     ],
     loaders: [
-        {
-            test: /\.jsx?$/,
-            loader: "babel",
-            include: [
-                path.join(__dirname, "src")
-            ]
-        },
-        {test: /\.css$/,loader: "style!css"},
-        {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-        {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-        {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        // {
+        //     test: /\.jsx?$/,
+        //     loader: "babel",
+        //     include: [
+        //         path.join(__dirname, "src")
+        //     ]
+        // },
+        // {test: /\.css$/,loader: "style!css"},
+        // {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+        // {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+        // {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+        // {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
 };
