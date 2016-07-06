3a3cbf3ad7d18caa25713f73f6c49f24c9ca395e
Alex Perfilov <alexandr.perfilov@reisystems.com>
2016-07-05 23:48:41 -0400

    refactored

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,5 @@ module.exports = {
     },
     plugins: [
         new webpack.IgnorePlugin(/^(buffertools)$/)
-    ],
-    loaders: [
-        // {
-        //     test: /\.jsx?$/,
-        //     loader: "babel",
-        //     include: [
-        //         path.join(__dirname, "src")
-        //     ]
-        // },
-        // {test: /\.css$/,loader: "style!css"},
-        // {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-        // {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-        // {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-        // {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
 };
