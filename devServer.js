d9a37854b5368fb5030f90ecfe1c3b557166ffae
Alex Perfilov <alexandr.perfilov@reisystems.com>
2016-07-05 23:56:15 -0400

    known issue added

--- devServer.js
+++ devServer.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.static('bin'));
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Dev version of the app listening on port 3000!');
 });
